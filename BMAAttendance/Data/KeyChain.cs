1f1be0aa6e668402532908ce7446708a8b98ee25
Alejandro Enriquez <prions48@gmail.com>
2025-05-02 20:16:07 -0500

    init from v3 template

---
+++ BMAAttendance/Data/KeyChain.cs
@@ -0,0 +1,42 @@
+namespace BMAAttendance.Data
+{
+    /// <summary>
+    /// In-app settings not needed to be abstracted to user secrets
+    /// </summary>
+    public static class KeyChain
+    {
+        public static string AppName
+        {
+            get
+            {
+                return "BMA Attendance Tracker";
+            }
+        }
+        public static string AppCode
+        {
+            get
+            {
+                return "BMAAttendance";
+            }
+        }
+        public static string Container
+        {
+            get
+            {
+                return "bmafilestorage";
+            }
+        }
+        public static string OwnerEmail
+        {
+            get
+            {
+                return "prions48@gmail.com";
+            }
+        }
+    }
+    public enum Environ
+    {
+        MainApp
+    }
+
+}
