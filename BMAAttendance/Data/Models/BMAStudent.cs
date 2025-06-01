74a61ad746459e018ffbe27c3101ae8188f4beb8
Alejandro Enriquez <prions48@gmail.com>
2025-06-01 13:49:26 -0500

    added gup #

--- BMAAttendance/Data/Models/BMAStudent.cs
+++ BMAAttendance/Data/Models/BMAStudent.cs
@@ -26,6 +26,7 @@ namespace BMAAttendance.Data.Models
         public string LastName { get; set; } = "";
         public string EmailAddress { get; set; } = "";
         public string? PhoneNumber { get; set; }
+        public string? GupNumber { get; set; }
         public Guid? RankID { get; set; }
         [NotMapped] public BMARank? CurerntRank { get; set; }//set in dialog
         public DateTime? DateAwarded { get; set; }
